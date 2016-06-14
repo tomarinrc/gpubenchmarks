e3f4a5175ee378f889891084004bf1d0cf1a3d3d
tomarinrc <tarce@ibg.local>
2016-06-14 16:07:53 -0500

    Added Chart.JS to Project
    -Featured Chart on HomePage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,76 @@
 $(function() {
-	$(".button-collapse").sideNav();
+  $(".button-collapse").sideNav();
+
+  //Test Chart.JS
+  Chart.defaults.global.defaultFontFamily = "Roboto";
+  Chart.defaults.global.defaultFontSize = 14;
+
+  var topCardsChartContainer = $("#TopCards");
+  var bangCardsChartContainer = $("#BangCards");
+  var topCardsChart = new Chart(topCardsChartContainer, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["NVIDIA GTX 980 Ti", "AMD RX 480", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: 'Score',
+        data: [1000, 640, 400, 200, 110, 20],
+        backgroundColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 0
+      }]
+    },
+
+    options: {
+      legend: {
+        display: false,
+      }
+      /*
+          title: {
+            display: true,
+            text: 'Custom Chart Title'
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          },*/
+    }
+  });
+  var bangCardsChart = new Chart(bangCardsChartContainer, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["NVIDIA GTX 980 Ti", "AMD RX 480", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: 'Score',
+        data: [1000, 640, 400, 200, 110, 20],
+        backgroundColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 0
+      }]
+    },
+
+    options: {
+      legend: {
+        display: false,
+      }
+      /*
+      		title: {
+      			display: true,
+      			text: 'Custom Chart Title'
+      		},
+      		scales: {
+      			yAxes: [{
+      				ticks: {
+      					beginAtZero: true
+      				}
+      			}]
+      		},*/
+    }
+  });
 });
